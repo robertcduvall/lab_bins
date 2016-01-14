3930100437533cc73839fa6873d3dcf0a3fb439e
Adam Tache <at201@duke.edu>
2016-01-14 16:46:26 -0500

    Small code change

--- src/Disk.java
+++ src/Disk.java
@@ -73,9 +73,8 @@ public class Disk implements Comparable<Disk> {
         if (other != null && other instanceof Disk) {
             if (myId == ((Disk) other).myId) {
                 return true;
-            } else {
+            } else
                 return false;
-            }
         } else {
             return false;
         }
